74e454efe5e1b11d1633126c7018421b054964bd
TatianaDEV7 <98289003+TatianaDEV7@users.noreply.github.com>
2023-08-05 18:39:25 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,48 @@
-''' выбери мод: 0 - минт в одной сети (и сразу chain), 1 - минт в рандомной сети В КОТОРОЙ ЕСТЬ НАТИВНЫЕ ТОКЕНЫ НА ГАЗ(bsc, polygon, avax)'''
-import random
-mode = 1
-count_nfts = (1,1)  #cтавим либо определенное количество (1,1) либо от скольки рандомно, до скольки рандомно
-CHAIN = 'opti'  #avax, polygon, bsc, opti
+"""
 
-c = random.randint(1,1)  #ставим от и до (сколько кошельков будет рандомно в пачке, то есть если всего 20 и будет выбрано 2 то будет 10 пачек кошельков)
+count  - количество нфт на кошелек для минта - либо статичное число либо - [от, до] штук
 
-#cтавим от скольки до скольки секунд рандомно будет задержка между пачками
+MODE - bridger, minter
 
-start = 0 #от скольки
-end = 0  #до скольки секунд
+minter_mode / bridger_mode = поиск сети с балансом / поиск сети с нфт - и выбор сети : режим 1, или своя сеть : 0 (ставь сразу chain)
+
+chain, to_chain - cети, если хотите выбрать рандом сеть для бриджа в нее то юзаем так ['cеть', 'cеть'] и тд
+
+shuffle - мешаем кошельки - 1, не мешаем - 0
+
+delay - от и до в секундах
+
+moralis_api - https://admin.moralis.io/settings идем сюда и берем default ключ
+
+rpcs - Небольшая рекомендация для тех кто будет юзать данный скрипт, также для комфортной работе на большом количестве аккаунтов
+рекомендую юзать приватные рпц, купить можно на любом из этих сервисов - nodereal (https://nodereal.io/),infura (https://www.infura.io/)
+
+"""
+
+
+count = 1  # [от, до]
+
+MODE = ''  # bridger, minter
+
+minter_mode = 1  # 0 - your chain
+
+bridger_mode = 1  # 0 - your chain
+
+
+# bsc, avax, polygon, opti
+
+chain = ''
+
+to_chain = ''  # ['your chain', 'your chain'...] #бридж только в bsc, avax, polygon !!! из bsc, avax, polygon, opti
+
+shuffle = 1
+
+delay = (0, 100)
+
+moralis_api = ''
+
+rpcs = {'bsc': 'https://bscrpc.com',
+        'polygon': 'https://rpc.ankr.com/polygon',
+        'avax': 'https://rpc.ankr.com/avalanche',
+        'opti': 'https://rpc.ankr.com/optimism',
+        }
